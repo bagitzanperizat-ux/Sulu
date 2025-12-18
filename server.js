d0b45ddda0fa91ba49fd8dfbe2d64d26c5c4bf48
Aida Bekpayeva <aidabekpayeva@example.com>
2025-12-18 18:15:22 +0500

    server.js changed, information in about.html added

--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 const express = require('express');
+const path = require('path'); 
 const app = express();
 
-app.use(express.static('public'));
+// Middleware to parse form data
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'about.html'));
+});
+
+// Contact page (GET)
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'contact.html'));
+});
+
+// Contact form submission (POST)
+app.post('/contact', (req, res) => {
+  console.log('Form submission:', req.body);
+  const { name } = req.body;
+
+  res.send(`<h2>Thanks, ${name}! Your message has been received.</h2>`);
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000, () => {
+  console.log(`Server running at http://localhost:3000`);
+});
